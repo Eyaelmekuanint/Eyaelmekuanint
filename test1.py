eefc8cbe7766936ff50eee2ca760bd5ee200fbe6
Eyaelmekuanint <ja.el1200zek@gmail.com>
2024-08-12 18:20:32 +0300

    Calculator.py

--- test1.py
+++ test1.py
@@ -1,2 +1,16 @@
-print("hello")
-print("world")
+x = (input("Enter an operator (+ - × /) : "))
+a= float(input(" Enter the first number : "))
+b=float(input("Enter the second number: "))
+
+if x=="+" :
+    s=a+b
+elif x== "-":
+    s=a-b
+elif x== "*":
+    s=a*b
+elif x== "/":
+    s=a/b
+else:
+    print("invalid operator ")
+print(s)
+
