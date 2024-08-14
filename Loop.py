7e37cffd54705660af2cc567f3c97184e97ac678
Eyaelmekuanint <ja.el1200zek@gmail.com>
2024-08-14 08:11:45 +0300

    Updated1 loop.py

--- Loop.py
+++ Loop.py
@@ -5,3 +5,19 @@ while i!=5:
     fact*=n-i
     i+=1
 print(fact) 
+
+
+x="abcdefghijklmnop"
+i=0
+let=" "
+while x[i]!="o":
+    if x[i]=="a":
+        let="a"
+    elif x[i]=="m":
+        let+="m"
+    elif x[i]=="j":
+        let+="j"
+if len(let)==3:
+    print("true")
+else:
+    print("false")
